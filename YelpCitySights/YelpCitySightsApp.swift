072fc6e1725e8d878b81a07950d4af808fd947ff
iDevArya <aryashiva1126@gmail.com>
2024-04-16 22:34:01 -0400

    Initial Commit

---
+++ YelpCitySights/YelpCitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpCitySightsApp.swift
+//  YelpCitySights
+//
+//  Created by Shiva kumar Arya on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct YelpCitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
